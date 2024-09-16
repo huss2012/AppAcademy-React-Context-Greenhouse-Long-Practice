7caf7fae5264206e3542a327723261fcc66e266f
huss2012 <huss24940@gmail.com>
2024-09-16 23:15:03 +0300

    ClimateContext has been set properly.

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,19 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { useState, useContext, createContext } from "react";
+//The context
+export const climateContext = createContext();
+//The custom hook
+export const useClimate = () => useContext(climateContext);
+
+//The provider
+export default function ClimateProvider({ children }){
+    const [temperature, setTemperature] = useState(50);
+
+    return (
+        <climateContext.Provider value={{ temperature, setTemperature }}>
+            {children}
+        </climateContext.Provider>
+    );
+}
